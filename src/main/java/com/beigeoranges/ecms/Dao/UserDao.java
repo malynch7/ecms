56cee02a786bb5be0d70365a25f5195cdc801d8f
Chang Yea Moon <changyeamoon@gmail.com>
2018-07-05 15:46:31 -0400

    added registration function

--- src/main/java/com/beigeoranges/ecms/Dao/UserDao.java
+++ src/main/java/com/beigeoranges/ecms/Dao/UserDao.java
@@ -10,6 +10,8 @@ import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 @Transactional
 public class UserDao extends JdbcDaoSupport {
@@ -29,6 +31,26 @@ public class UserDao extends JdbcDaoSupport {
             User userInfo = this.getJdbcTemplate().queryForObject(sql, params, mapper);
             return userInfo;
         } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    public void createUser(User user){
+        String sqlcreateuser ="INSERT INTO users (email, password, first_name, last_name, enabled) VALUE(?,?,?,?,?)";
+        String email = user.getUserName();
+        String password = user.getEncryptedPassword();
+        String first_name = user.getFirstName();
+        String last_name  = user.getLastName();
+
+        getJdbcTemplate().update(sqlcreateuser,new Object[]{email,password,first_name,last_name,1});
+    }
+
+    public List<User> getAllUsers(){
+        String sqlgetallusers = "SELECT * FROM users;";
+
+        try {
+            return getJdbcTemplate().query(sqlgetallusers, new UserMapper());
+        } catch (Exception e) {
             return null;
         }
     }
