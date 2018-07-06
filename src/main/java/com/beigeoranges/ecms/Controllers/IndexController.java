b60ca497b9c8d9d8593475978c03b617202dbeef
Mark Lynch <malynch7@gmail.com>
2018-07-06 18:51:14 -0400

    added Admin Code to registration page

--- src/main/java/com/beigeoranges/ecms/Controllers/IndexController.java
+++ src/main/java/com/beigeoranges/ecms/Controllers/IndexController.java
@@ -73,7 +73,7 @@ public class IndexController {
         return "player/dashboard";
     }
 
-    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    @RequestMapping(value = "/error/access-denied", method = RequestMethod.GET)
     public String accessDenied(Model model, Principal principal) {
 
         if (principal != null) {
@@ -89,7 +89,7 @@ public class IndexController {
 
         }
 
-        return "403Page";
+        return "error/access-denied";
     }
 
     @RequestMapping("/diverter")
