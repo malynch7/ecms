b5071449b48cbc4c27647926954bb0962128a66d
Mark Lynch <malynch7@gmail.com>
2018-07-14 19:49:29 -0400

    added view archived events to player dashboard

--- src/main/java/com/beigeoranges/ecms/Controllers/ArchivedEventController.java
+++ src/main/java/com/beigeoranges/ecms/Controllers/ArchivedEventController.java
@@ -1,6 +1,8 @@
 package com.beigeoranges.ecms.Controllers;
 
+import com.beigeoranges.ecms.Dao.ArchivedEventDao;
 import com.beigeoranges.ecms.Dao.EventDao;
+import com.beigeoranges.ecms.Dao.UserDao;
 import com.beigeoranges.ecms.Model.ArchivedEvent;
 import com.beigeoranges.ecms.Model.Event;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +20,29 @@ public class ArchivedEventController {
     @Autowired
     private EventDao eventDao;
 
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private ArchivedEventDao archivedEventDao;
+
     @RequestMapping(value = "admin/archiveEvent", method = RequestMethod.GET)
     public String viewArchiveEventPage(Model model) {
         ArchivedEvent aEvent = new ArchivedEvent();
         List<String> curEvents = eventDao.curEvents();
         model.addAttribute("Events", curEvents);
         return "admin/archiveEvent";
+    }
+
+    @RequestMapping(value = "player/viewArchived", method = RequestMethod.GET)
+    public String viewArchivedEvents(Model model, Principal principal) {
+
+        String userName = principal.getName();
+        int userId = Math.toIntExact((userDao.findUserAccount(userName)).getUserId());
+
+        List<Event> archivedEvents = archivedEventDao.getArchivedEvents(userId);
+        model.addAttribute("archivedEvents", archivedEvents);
+
+        return "player/viewArchived";
     }
 }
