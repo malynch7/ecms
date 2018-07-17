6d8f4f5eab040ce021081dc07c1189e40a2cac8c
stsegaye2 <stsegaye2@student.gsu.edu>
2018-07-16 23:33:58 -0400

    improved ArchivedEventDao

--- src/test/java/com/beigeoranges/ecms/Dao/ArchivedEventDaoTest.java
+++ src/test/java/com/beigeoranges/ecms/Dao/ArchivedEventDaoTest.java
@@ -1,7 +1,19 @@
 package com.beigeoranges.ecms.Dao;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ArchivedEventDaoTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void createArchivedEvent() {
+
+
+    }
 }
